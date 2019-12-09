dd05c07291fc56d859c424fa5a6eb3c0481de617
Grizzly2487 <52928837+Grizzly2487@users.noreply.github.com>
2019-12-09 16:02:37 -0500

    Quest Manager Script Changes
    -Changed the functions in the Script for Quest Manager to have a llist of string names for completed quests that the quest giver can check to see if the quests have been completed.

--- Assets/Scripts/Quests/QuestBuilds/QuestManager.cs
+++ Assets/Scripts/Quests/QuestBuilds/QuestManager.cs
@@ -11,21 +11,33 @@ using UnityEngine;
 //PURPOSE :Checks to see what Quests have been done and what quests can be unlocked because of the completed quests. 
 public class QuestManager
 {
-    public bool isCompleted { get; set; }
-    public Rescue rescueQuest;
-    public Slayer slayerQuest;
-    public WinterIsComing wicQuest;
-    //FUNCTION : whenComplete()
-    //DESCRIPTION : Checking the completed status and assigning the available quests.
+    public List<string> completedQuestNames;
+    private bool isMatch = false;
+    //FUNCTION : searchCGNList()
+    //DESCRIPTION : Searchs the completed Quest Names list.
     //PARAMETERS : 
     //RETURNS : None()
-    public void whenComplete()
+    public void Awake()
     {
-        //if (rescueQuest.Completed)
-        //{
-        //    slayerQuest.currentQuest = true;
-        //}
-
-        //will link quests this way when they are all setup to be linked together
+        completedQuestNames[0] = "NULL";
+    }
+    public bool searchCQNList(string name)
+    {
+        for (int i = 0; i < completedQuestNames.Count; i++)
+        {
+            if(name == completedQuestNames[i])
+            {
+                isMatch = true;
+            }
+        }
+        return isMatch;
+    }
+    //FUNCTION : addCGNList()
+    //DESCRIPTION : Adds to the completed Quest Names list.
+    //PARAMETERS : 
+    //RETURNS : None()
+    public void addToCQNList(string addName)
+    {
+        completedQuestNames.Add(addName);
     }
 }
